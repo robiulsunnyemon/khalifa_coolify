b2ebc1f73798b7bb48df6067606c9ed17e0af7c0
robiulsunnyemon <robiulsunyemon@gmail.com>
2025-09-23 09:37:37 +0600

    add database for coolify

--- app/db/db.py
+++ app/db/db.py
@@ -6,14 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
-# engine = create_engine(DATABASE_URL)
-# Session = sessionmaker(bind=engine)
-
-
-database_url = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-engine = create_engine(database_url, echo=True)
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
+
+
+# database_url = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+# engine = create_engine(database_url, echo=True)
+# Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
