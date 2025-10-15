50a0f0d283c7c42bec62c0d9ce7ed1de787f1c87
robiulsunnyemon <robiulsunyemon@gmail.com>
2025-10-15 15:09:59 +0600

    update login for admin

--- app/routers/user.py
+++ app/routers/user.py
@@ -95,6 +95,26 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = 
 
 
 
+@router.post("/login_for_admin", status_code=status.HTTP_200_OK)
+
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    db_user = db.query(UserModel).filter(UserModel.email == form_data.username).first()
+    if db_user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    if not verify_password(form_data.password, db_user.password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong password")
+
+    if not db_user.is_verified:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You are not verified, please check your email")
+
+    if not db_user.role=="admin":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You are not an admin")
+
+    token = create_access_token(data={"sub": db_user.email, "role": db_user.role, "user_id": db_user.id})
+    return {"access_token": token, "token_type": "bearer"}
+
+
 
 @router.post("/resend-otp", status_code=status.HTTP_200_OK)
 async def resend_otp(user:ResendOTP,db: Session = Depends(get_db)):
