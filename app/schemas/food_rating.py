33f78bb83059f4ef425ff37d497238b3e2440c3b
robiulsunnyemon <robiulsunyemon@gmail.com>
2025-10-16 12:30:22 +0600

    update popular product

--- app/schemas/food_rating.py
+++ app/schemas/food_rating.py
@@ -1,8 +1,6 @@
 from pydantic import BaseModel,ConfigDict
 from typing import Optional,List
 
-from app.schemas.category_for_products import Product
-
 
 class FoodRatingCreate(BaseModel):
     food_id: int
@@ -16,23 +14,29 @@ class FoodRatingResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
 
+class VariationOfFoodResponse(BaseModel):
+    name: Optional[str]
+    price: Optional[float]
+
+
+
 class FoodBase(BaseModel):
     name: str
     description: Optional[str] = None
     food_image_url: Optional[str] = None
     price: float
     category_id: int
+    variations: List[Optional[VariationOfFoodResponse]] = []
 
-class VariationOfFoodResponse(BaseModel):
-    name: Optional[str]
-    price: Optional[float]
+
+
 
 
 class FoodRatingResponseForPopularFood(BaseModel):
     food_id: int
     average_rating: float
-    ##food: Optional[FoodBase]
-    food: List[Optional[Product]]=[]
+    food: Optional[FoodBase]
+
 
 
     model_config = ConfigDict(from_attributes=True)
